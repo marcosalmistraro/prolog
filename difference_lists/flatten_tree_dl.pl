b32017d32c80fdb340df3449054182cd869b479b
marcosalmistraro <marcosalmistraro@gmail.com>
2021-12-11 23:37:47 +0100

    added flatten_tree_dl.pl

---
+++ difference_lists/flatten_tree_dl.pl
@@ -0,0 +1,6 @@
+/* flattening a tree using difference lists */
+
+flatten_tree_dl(nil, L-L).
+flatten_tree_dl(t(L, X, R), L3-L1):-
+    flatten_tree_dl(L, L1-L2),
+    flatten_tree_dl(R, [X|L2]-L3).
