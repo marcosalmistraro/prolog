b356e689f25096348ff3f554db1a88106c794b8f
marcosalmistraro <marcosalmistraro@gmail.com>
2022-01-25 12:57:32 +0100

    added more binary tree scripts

---
+++ binary_trees/in_tree.pl
@@ -0,0 +1,12 @@
+/* predicate intree/2 to verify whether a given element is contained in an ordered binary tree.
+An ordered binary tree is a rooted binary tree data structure:
+the internal nodes all store a key greater than all the keys in the their left subtree;
+the key also has to be less than the keys in the right subtree. */
+
+intree2(V, t(_,V,_)):- !.
+intree2(V, t(L,X,_)):-
+    V < X,
+    intree2(V, L), !.
+intree2(V, t(_,X,R)):-
+    V > X,
+    intree2(V, R), !.
---
+++ binary_trees/is_balanced.pl
@@ -0,0 +1,10 @@
+/* is_balanced/1 predicate to verify whether a given binary tree is balanced.
+Balanced means that the depths of the left and right subtrees at any node differ by at most 1 */
+
+is_balanced(nil).
+is_balanced(t(L, _, R)):-
+    depth(L, DepthL),
+    depth(R, DepthR),
+    abs(DepthR - DepthL) =< 1,
+    is_balanced(L),
+    is_balanced(R), !.
