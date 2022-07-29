935dcf00e28704fbd8d01a285887c0199b60fead
Marco Salmistraro <marco@MacBook-Pro-di-Marco.local>
2022-07-29 10:50:42 +0200

    added exact_covering.pl

---
+++ exact_covering/exact_covering.pl
@@ -0,0 +1,76 @@
+/* 
+exact covering problem
+
+- items to be covered are represented as a list
+e.g. [a, b, c]
+
+- the set of options is a list of lists
+e.g. [[a, c], [e], [a, b]]
+
+*/
+
+isCovered(I, Os, O):-
+    member(O, Os),
+    member(I, O).
+
+getResIs(Is, O, ResIs):-
+    % remove all I contained in O from Is to get ResIs
+    findall(IToRemove, member(IToRemove, O), IsToRemove),
+    findall(ResI, (member(ResI, Is), not(member(ResI, IsToRemove))), ResIs).
+    
+residual(I, Is, O, Os, ResIs, ResOs):-
+    member(I, Is),
+    isCovered(I, Os, O),
+    getResIs(Is, O, ResIs),
+    select(O, Os, ResOs).
+
+% make sure that no element in O is contained in any of the sublists in Os
+
+% -- flatten list of lists (Covering) --
+
+flatten_Os(Os, FlatOs):-
+    findall(Element,
+        (member(O, Os),
+        member(Element, O)),
+        FlatOs).
+
+% no_intersection predicate
+
+no_intersection([O], Os):-
+    flatten_Os(Os, FlatOs),
+    not(member(O, FlatOs)).
+
+no_intersection([H|T], Os):-
+    flatten_Os(Os, FlatOs),
+    not(member(H, FlatOs)),
+    no_intersection_flat(T, FlatOs).
+
+% implement case when Os is already flattened
+
+no_intersection_flat([H], FlatOs):-
+    not(member(H, FlatOs)).
+
+no_intersection_flat([H|T], FlatOs):-
+    not(member(H, FlatOs)),
+    no_intersection_flat(T, FlatOs).
+
+% implement recursive search for disjunct exact covering Os
+
+findexactcovering(Is, Os, [O]):-
+    member(I, Is),
+    isCovered(I, Os, O),
+    residual(I, Is, O, Os, [], _).
+
+findexactcovering(Is, Os, [O|Covering]):-
+    member(I, Is),
+    isCovered(I, Os, O),
+    residual(I, Is, O, Os, ResIs, ResOs),
+    findexactcovering(ResIs, ResOs, Covering),
+    no_intersection(O, Covering).
+
+% only find unique covering solutions
+
+findallcoverings(Is, Os, Coverings):-
+    setof(Covering,
+        findexactcovering(Is, Os, Covering),
+        Coverings).
