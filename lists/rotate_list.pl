8d28cc7451bf7f34f76bb8d7d2fbc0134f33f696
Marco Salmistraro <marco@MBP-di-Marco.home>
2022-08-13 17:15:44 +0200

    added more list exercises

---
+++ lists/rotate_list.pl
@@ -0,0 +1,16 @@
+% rotate a given list by N places to the left
+
+rotateList(N, List, Res):-
+    splitList(N, List, Res1, Res2),
+    append(Res2, Res1, Res).
+
+splitList(N, List, Res1, Res2):-
+    isSublist(Res1, List),
+    length(Res1, N),
+    append(Res1, Res2, List).
+
+isSublist([], []).
+isSublist([H|T], [H|List]):-
+    isSublist(T, List).
+isSublist(T, [_|List]):-
+    isSublist(T, List).
